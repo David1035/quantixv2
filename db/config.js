c6c7e83430195dd9b692bd430366d2f71d4fcf9b
David Hernandez <davyd2h@gmail.com>
2025-09-02 15:24:42 -0500

    código actual, funciona perfecto creando la migrations a postgres

--- db/config.js
+++ db/config.js
@@ -1,38 +1,38 @@
-//const { config } = require('../config/config')
+const { config } = require('../config/config')
 
-// const USER = encodeURIComponent(config.dbUser);
-// const PASSWORD = encodeURIComponent(config.dbPassword);
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
 
-// const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-
-// module.exports = {
-//   development: {
-//     url: URL,
-//     dialect: 'postgres',
-//   },
-//   production: {
-//     url: URL,
-//     dialect: 'postgres',
-//   }
-// }
-
-
-// const { Sequelize } = require('sequelize');
-
-const path = require('path');
 
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: path.join(__dirname, '../db/database.sqlite') // o donde quieras guardar tu .sqlite
-  },
-  test: {
-    dialect: 'sqlite',
-    storage: ':memory:'
+    url: URL,
+    dialect: 'postgres',
   },
   production: {
-    dialect: 'sqlite',
-    storage: path.join(__dirname, '../db/database.sqlite')
+    url: URL,
+    dialect: 'postgres',
   }
-};
+}
+
+
+const { Sequelize } = require('sequelize');
+
+// const path = require('path');
+
+// module.exports = {
+//   development: {
+//     dialect: 'sqlite',
+//     storage: path.join(__dirname, '../db/database.sqlite') // o donde quieras guardar tu .sqlite
+//   },
+//   test: {
+//     dialect: 'sqlite',
+//     storage: ':memory:'
+//   },
+//   production: {
+//     dialect: 'sqlite',
+//     storage: path.join(__dirname, '../db/database.sqlite')
+//   }
+// };
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,38 +1,37 @@
 const { Sequelize } = require('sequelize');
-const path = require('path');
-//const setupModel = require('./../db/models')
-// const { config } = require('../config/config')
+const setupModel = require('./../db/models')
+const { config } = require('../config/config')
 
-// const USER = encodeURIComponent(config.dbUser);
-// const PASSWORD = encodeURIComponent(config.dbPassword);
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
 
-// const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const URL = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
-// const sequelize = new Sequelize (URL, {
-//   dialect: 'postgres',
-//   logging: true
-// })
+const sequelize = new Sequelize (URL, {
+  dialect: 'postgres',
+  logging: true
+})
 
 
-// setupModel(sequelize);
-
-//module.exports = sequelize;
-
-const path = require('path');
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, '../database.sqlite'),
-  logging: false
-});
-
-// Habilitar foreign_keys después de la conexión
-sequelize.authenticate().then(() => {
-  return sequelize.query('PRAGMA foreign_keys = ON;');
-}).then(() => {
-  console.log('Foreign keys enabled');
-}).catch(err => {
-  console.error('Unable to enable foreign keys:', err);
-});
+setupModel(sequelize);
 
 module.exports = sequelize;
+
+// const path = require('path');
+
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: path.join(__dirname, '../database.sqlite'),
+//   logging: false
+// });
+
+// // Habilitar foreign_keys después de la conexión
+// sequelize.authenticate().then(() => {
+//   return sequelize.query('PRAGMA foreign_keys = ON;');
+// }).then(() => {
+//   console.log('Foreign keys enabled');
+// }).catch(err => {
+//   console.error('Unable to enable foreign keys:', err);
+// });
+
+// module.exports = sequelize;
