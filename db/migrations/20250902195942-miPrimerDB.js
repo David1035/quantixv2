677f679b36ae598c4579567f924d8e1a642c7d24
David Hernandez <davyd2h@gmail.com>
2025-09-25 06:54:00 -0500

    migrations:run y revert funcionan ok, orden de ejecución

--- db/migrations/20250902195942-miPrimerDB.js
+++ db/migrations/20250902195942-miPrimerDB.js
@@ -7,7 +7,8 @@ const { CreditModel, CREDIT_TABLE } = require('./../models/credit.model');
 const { CreditPaymentsModel, CREDIT_PAYMENTS_TABLE } = require('./../models/credit-payments.model');
 const { CategoryModel, CATEGORY_TABLE } = require('./../models/category.model');
 const { ProductModel, PRODUCT_TABLE } = require('./../models/product.model');
-const { PRODUCT_PROVEEDOR_TABLE, productProveedorModel } = require('./../models/product-proveedor.model')
+const { ProductSupplierModel, PRODUCT_SUPPLIER_TABLE } = require('./../models/product-supplier.model');
+const { SupplierModel, SUPPLIER_TABLE } = require('./../models/supplier.model');
 
 
 /** @type {import('sequelize-cli').Migration} */
@@ -20,31 +21,10 @@ module.exports = {
     await queryInterface.createTable(CREDIT_PAYMENTS_TABLE, CreditPaymentsModel);
     await queryInterface.createTable(CATEGORY_TABLE, CategoryModel);
     await queryInterface.createTable(PRODUCT_TABLE, ProductModel);
+    await queryInterface.createTable(SUPPLIER_TABLE, SupplierModel);
+    await queryInterface.createTable(PRODUCT_SUPPLIER_TABLE, ProductSupplierModel);
 
 
-  },
-
-  async down (queryInterface) {
-    await queryInterface.dropTable(CREDIT_PAYMENTS_TABLE);
-    await queryInterface.dropTable(CREDIT_TABLE);
-    await queryInterface.dropTable(CUSTOMER_TABLE);
-    await queryInterface.dropTable(PROFILE_TABLE);
-    await queryInterface.dropTable(USER_TABLE);'use strict';
-
-const { UserModel, USER_TABLE } = require('./../models/user.model');
-const { ProfileModel, PROFILE_TABLE } = require('./../models/profile.model');
-const { CustomerModel, CUSTOMER_TABLE } = require('./../models/customer.model');
-const { CreditModel, CREDIT_TABLE } = require('./../models/credit.model');
-const { CreditPaymentsModel, CREDIT_PAYMENTS_TABLE } = require('./../models/credit-payments.model');
-
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface) {
-    await queryInterface.createTable(USER_TABLE, UserModel);
-    await queryInterface.createTable(PROFILE_TABLE, ProfileModel);
-    await queryInterface.createTable(CUSTOMER_TABLE, CustomerModel);
-    await queryInterface.createTable(CREDIT_TABLE, CreditModel);
-    await queryInterface.createTable(CREDIT_PAYMENTS_TABLE, CreditPaymentsModel);
   },
 
   async down (queryInterface) {
@@ -53,14 +33,10 @@ module.exports = {
     await queryInterface.dropTable(CUSTOMER_TABLE);
     await queryInterface.dropTable(PROFILE_TABLE);
     await queryInterface.dropTable(USER_TABLE);
+    await queryInterface.dropTable(PRODUCT_SUPPLIER_TABLE);
     await queryInterface.dropTable(PRODUCT_TABLE);
-    await queryInterface.dropTable(CATEGORY_TABLE);
-
-
+    await queryInterface.dropTable(SUPPLIER_TABLE);
   }
-};
 
 
-
-  }
 };
