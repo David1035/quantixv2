83ba747eaa9f2f09035c986072a18d018b8ef902
David Hernandez <davyd2h@gmail.com>
2025-10-01 09:47:33 -0500

    aquí eliminamos la contraseña de la respuesta que enviamos en el request. y Además, se personaliza para enviar desde postman email, cuando se envian los campos

--- utils/auth/strategy/local.strategy.js
+++ utils/auth/strategy/local.strategy.js
@@ -5,21 +5,29 @@ const boom = require('@hapi/boom');
 
 const service = new UserService();
 
-const LocalStrategy = new Strategy(async (email, password, done) => {
-  try {
-    const user = await service.findByEmail(email);
-    if(!user){
-      done(boom.unauthorized(), false);
-    }
+const LocalStrategy = new Strategy({
+  //cambiar el nombre de por defecto a los campos.
+  usernameField: 'email',
+  passwordField: 'password'
+}, async (email, password, done) => {
+    try {
+      const user = await service.findByEmail(email);
+      if(!user){
+        done(boom.unauthorized(), false);
+      }
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if(!isMatch){
-      done(boom.unauthorized(), false);
+      const isMatch = await bcrypt.compare(password, user.password);
+      if(!isMatch){
+        done(boom.unauthorized(), false);
+      }
+
+      delete user.dataValues.password;
+
+      done(null, user);
+    } catch (error) {
+      done(error, false);
     }
-    done(null, user);
-  } catch (error) {
-    done(error, false);
   }
-})
+)
 
 module.exports = LocalStrategy;
